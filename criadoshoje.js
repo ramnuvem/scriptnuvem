d2d771307e85f8dda243e3633ccf158c521fd66a
ramnuvem <99828501+ramnuvem@users.noreply.github.com>
2022-02-16 14:53:24 -0300

    Teste

--- criadoshoje.js
+++ criadoshoje.js
@@ -1,6 +1,6 @@
 
 function criadoshoje(url_atual) {
-	alert('criadoshoje');
+	alert('Teste criadoshoje');
 	var url = url_atual.split('#D')[0];
 	url = url + '_PublishedToday_YES';
 	window.open(url, "_blank");
